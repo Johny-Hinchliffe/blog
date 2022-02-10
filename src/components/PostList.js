53ef61609738b4b631525ebbcf3ca0205e82840a
Johny-Hinchliffe <82289369+Johny-Hinchliffe@users.noreply.github.com>
2022-02-10 08:09:55 +0000

    collected all the users but too much

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader'
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      // this.props.fetchUser(post.id);
+      // console.log(this.props.user)
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+            <div className="content">
+              <div className="description">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+              <UserHeader userId={post.userId}/>
+            </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props.posts)
-    return <div>Post List</div>;
+    return <div className="ui divided relaxed list">{this.renderList()}</div>;
   }
 }
 
+//state === combineReducers 
+// essentially posts = allReducers.posts 
 const mapStateToProps = (state) => {
   return {posts: state.posts}
 }
 
 export default connect(
   mapStateToProps,
-  { fetchPosts }
+  { fetchPosts}
 )(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import postReducers from './postReducers'
+import usersReducers from './usersReducers'
+
 
 export default combineReducers({
-  posts: postReducers
+  posts: postReducers,
+  users: usersReducers
 });
